b3f29bff2828f79dea85abab5a782a0542fd52ad
Ahmed_Salah <ahmedas24630@gmail.com>
2026-09-06 12:46:45 +0300

    fixed compilation errors

--- Src/App_Transitions/State_Context.cpp
+++ Src/App_Transitions/State_Context.cpp
@@ -1,11 +1,11 @@
 #include "State_Context.hpp"
+#include <atomic>
 
 
 class context {
-    state* current;
-    void handle_event(Event e){
-        current = current->handle(e);
-    }
+    public:
+        state* current;
+        void handle_event(Event e){current = current->handle(e);}
 };
 
 std::atomic<bool> g_engine_running(true);
@@ -14,7 +14,7 @@ void state_machine_engine() {
     context ctx; // The State Machine lives entirely inside this function!
     while (true) {
         // Thread safely freezes right here, consuming 0% CPU power until nudged
-        Event requested_event = EVENT_RUNNING;//g_mailbox.pop(g_engine_running);
+        Event requested_event = Event::EVENT_RUNNING;//g_mailbox.pop(g_engine_running);
         
         ctx.handle_event(requested_event);
     }
