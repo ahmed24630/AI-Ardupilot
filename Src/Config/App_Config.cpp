fce134d4d89400846805877920cc5902ae91c03f
Ahmed_Salah <ahmedas24630@gmail.com>
2026-09-26 11:35:59 +0300

    updated to use udpin in Mavsdk

--- Src/Config/App_Config.cpp
+++ Src/Config/App_Config.cpp
@@ -7,10 +7,11 @@ mavsdk::Mavsdk mavobj(config);
 
 void StartCommunication(std::string connection_type, double port, double timeout_seconds){
 
-    mavsdk::ConnectionResult Commstatus = mavobj.add_any_connection(connection_type + "://:" + std::to_string(port));
+    mavsdk::ConnectionResult Commstatus = mavobj.add_any_connection(connection_type + "://0.0.0.0:" + std::to_string(port));
 
     if(Commstatus != mavsdk::ConnectionResult::Success){
         std::cout << "Failed to connect: code " << static_cast<int>(Commstatus) << std::endl;
+        std::cout << "Failed to connect: " << mavsdk::to_string(Commstatus) << std::endl;
         return;
     }
 
